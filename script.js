5b4d83e998a2048ac559179024584c1e6d7cc20a
Void <Romeluswansschuster@gmail.com>
2025-03-06 20:22:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,4 @@ updateHistory();
 
 // Function to pull gacha
 function pullGacha() {
-    let result = characters[Math.floor(Math.random() * characters.length)];
-
-    // Save result
-    pulls.push(result);
-    localStorage.setItem("gachaPulls", JSON.stringify(pulls));
-
-    // Show result
-    document.getElementById("result").innerText = "You got: " + result;
-    updateHistory();
-}
-
-// Function to update pull history
-function updateHistory() {
-    let history = document.getElementById("history");
-    history.innerHTML = "";
-    
-    pulls.forEach(pull => {
-        let li = document.createElement("li");
-        li.innerText = pull;
-        history.appendChild(li);
-    });
-}
+    let result = characters[Math.
